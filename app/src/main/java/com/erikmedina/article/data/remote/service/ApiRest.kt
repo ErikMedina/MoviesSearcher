3bd3dc8751c3ed66be081cf770a53189da06bd2b
Erik Medina <erik.medina.chanca@gmail.com>
2018-07-22 11:10:14 +0200

    Add content list request

--- app/src/main/java/com/erikmedina/article/data/remote/service/ApiRest.kt
+++ app/src/main/java/com/erikmedina/article/data/remote/service/ApiRest.kt
@@ -1,17 +1,14 @@
 package com.erikmedina.article.data.remote.service
 
+import com.erikmedina.article.data.remote.model.ContentListResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 /**
  * The API for all the requests of the application
  */
 interface ApiRest {
 
-//    @GET("data/2.5/group")
-//    fun getCurrentCitiesTemperature(@Query("appid")appId: String,
-//                                    @Query("id")id: String,
-//                                    @Query("units")units: String
-//    ): Call<Group>
+    @GET("test/native/contentList.json")
+    fun getContentList(): Call<ContentListResponse>
 }
