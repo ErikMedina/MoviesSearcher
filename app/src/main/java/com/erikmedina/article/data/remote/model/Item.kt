12f19eac9a529e9927c91c4720c071ac6ca7e58e
Erik Medina <erik.medina.chanca@gmail.com>
2018-07-22 11:10:57 +0200

    Update item's models and add a mapper

--- app/src/main/java/com/erikmedina/article/data/remote/model/Item.kt
+++ app/src/main/java/com/erikmedina/article/data/remote/model/Item.kt
@@ -1,5 +1,6 @@
 package com.erikmedina.article.data.remote.model
 
+import com.erikmedina.article.util.Constant
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
@@ -8,17 +9,17 @@ class Item {
 
     @SerializedName("id")
     @Expose
-    var id: Int? = null
+    var id: Int = -1
     @SerializedName("title")
     @Expose
-    var title: String? = null
+    var title: String = Constant.UNKNOWN
     @SerializedName("subtitle")
     @Expose
-    var subtitle: String? = null
+    var subtitle: String = Constant.UNKNOWN
     @SerializedName("body")
     @Expose
-    var body: String? = null
+    var body: String = Constant.UNKNOWN
     @SerializedName("date")
     @Expose
-    var date: String? = null
+    var date: String = Constant.UNKNOWN
 }
