c02264f557d0425e4ac7de9b7c7f30e6a6508139
Erik Medina <erik.medina.chanca@gmail.com>
2018-07-22 09:36:08 +0200

    Initialize API Rest

---
+++ app/src/main/java/com/erikmedina/article/data/remote/service/RetrofitFactory.kt
@@ -0,0 +1,27 @@
+package com.erikmedina.article.data.remote.service
+
+import com.erikmedina.article.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+/**
+ * We only need one instance of Retrofit so we use Kotlin's Object to declare this
+ * class as a Singleton
+ */
+object RetrofitFactory {
+
+    fun createWebService(): ApiRest {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.API_ENDPOINT)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        return retrofit.create(ApiRest::class.java)
+    }
+}
